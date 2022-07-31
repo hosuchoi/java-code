eb056923d1f8f83b943d9ed459fa9bc95f74fa5d
lake <lake1310@gmail.com>
2022-07-31 14:06:15 +0900

    type of reflection

--- src/main/java/org/lake/reflection/App.java
+++ src/main/java/org/lake/reflection/App.java
@@ -2,14 +2,16 @@ package org.lake.reflection;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //type을 통한 접근
         Class<Book> bookClass = Book.class;
 
+        //인스턴스를 통한 접근
         Book book = new Book();
         Class<? extends Book> aClass = book.getClass();
 
-//        Class<?> aClass1 = Class.forName("org.lake.reflection.Book");
-
+        //문자열 내용뿐이 모를때는 FQCN (Full Qualified Class Name) 방식으로 접근
+        Class<?> aClass1 = Class.forName("org.lake.reflection.Book");
 
     }
 }
