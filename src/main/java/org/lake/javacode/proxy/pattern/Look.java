8d46e3adcd86cef1f15647af7e2de7ca01266b41
lake <lake1310@gmail.com>
2022-08-01 02:56:56 +0900

    proxy

---
+++ src/main/java/org/lake/javacode/proxy/pattern/Look.java
@@ -0,0 +1,31 @@
+package org.lake.javacode.proxy.pattern;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Look {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    private Integer id;
+
+    @Column
+    private String title;
+
+
+}
